e2e3bc5445309212930797aa85ed7325aaab1f3d
Daniel Loback <daniel.loback@gmail.com>
2022-06-13 12:37:54 -0300

    Versão inicial desafio Typescript

---
+++ TypeScript/d2.ts
@@ -0,0 +1,26 @@
+interface Pessoa {
+    nome: string,
+    idade: number,
+    profissao: string
+};
+
+const pessoaObj1: Pessoa = {
+    nome: 'Maria',
+    idade: 29,
+    profissao: 'Atriz'
+}
+const pessoaObj2: Pessoa = {
+    nome: 'Roberto',
+    idade: 19,
+    profissao: 'Padeiro'
+}
+const pessoaObj3: Pessoa = {
+    nome: 'Laura',
+    idade: 32,
+    profissao: 'Atriz'
+};
+const pessoaObj4: Pessoa = {
+    nome: 'Carlos',
+    idade: 19,
+    profissao: 'Padeiro'
+};
